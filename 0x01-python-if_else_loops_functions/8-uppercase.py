54941326657291636c648c35930028755c231e94
esmond-adjei <xmongram@gmail.com>
2023-03-08 11:47:41 -0800

    modified some files

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    up = ''
+    for c in str:
+        if ord(c) <= 123 and ord(c) >= 97:
+            up += chr(ord(c)-32)
+        else:
+            up += c
+    print(up)
