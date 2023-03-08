b82333e650607bf30ba441f80d73cff2776954e9
esmond-adjei <xmongram@gmail.com>
2023-03-08 12:18:19 -0800

    modified some files

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for c in range(97, 123):
-    if c == 122:
-        print("{0}".format(chr(c)))
-    else:
-        print("{0}".format(chr(c)), end='')
+    print("{0}".format(chr(c)), end='')
