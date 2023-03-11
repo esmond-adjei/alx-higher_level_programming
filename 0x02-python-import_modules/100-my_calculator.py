553b99488a12326bc68a12020d39310affcb5410
esmond-adjei <xmongram@gmail.com>
2023-03-10 19:19:11 -0800

    modified some files

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,19 +2,18 @@
 if __name__ == "__main__":
     from sys import argv, exit
     from calculator_1 import add, sub, div, mul
-    
     argc = len(argv)
     if argc - 1 < 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
     op = argv[2]
     a = int(argv[1])
     b = int(argv[3])
-    if op not in ("+", "-", "*", "/"):
+    func = {"+": add, "-": sub, "*": mul, "/": div}
+    if op not in func.keys():
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
-        func = add if op == "+" else (sub if op ==  "-" else (mul if op == "*" else div))
-    print("{:d} {} {:d} = {:d}".format(a, op, b, func(a,b)))
-    exit(0)
+        func = func[op]
+        print("{:d} {} {:d} = {:d}".format(a, op, b, func(a, b)))
+        exit(0)
