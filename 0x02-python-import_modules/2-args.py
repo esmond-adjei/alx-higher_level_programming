cfd72805e7c5e2c85f8623110c4ef85b75383b7d
esmond-adjei <xmongram@gmail.com>
2023-03-10 18:25:45 -0800

    modified some files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 if __name__ == '__main__':
     from sys import argv
     argc = len(argv)
-    if argc <=1:
+    if argc <= 1:
         print("{:d} arguments.".format(argc - 1))
     else:
         print("{:d} argument{}:".format(argc - 1, "s" if argc > 2 else ""))
